8555759f607944384b7e2a5b1afd5e330d45057b
vitharme <vithar@vithar.me>
2021-01-04 15:11:20 +0100

    move menu

--- menu.py
+++ menu.py
@@ -7,9 +7,10 @@ import RPi.GPIO as GPIO
 from pygame.locals import *
 os.putenv('SDL_FBDEV', '/dev/fb1')
 
+button_map = {23:(0,60), 24:(100,0)}
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+for k in button_map.keys():
+    GPIO.setup(k, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def main():
     pygame.init()
@@ -22,13 +23,11 @@ def main():
     GREY=(220,220,220)
     HGREY=(105,105,105)
     DISPLAY.fill(WHITE)
-
     font = pygame.font.SysFont('Arial', 35)
     # First
     pygame.draw.rect(DISPLAY,HGREY,(25,15,200,50))
     pygame.draw.rect(DISPLAY,GREY,(20,10,200,50))
     DISPLAY.blit(font.render('Gaona\'s Bell', True, RED), (25, 15))
-    FIRST='20,10'
     # Second
     pygame.draw.rect(DISPLAY,HGREY,(25,75,200,50))
     pygame.draw.rect(DISPLAY,GREY,(20,70,200,50))
@@ -37,17 +36,14 @@ def main():
     pygame.draw.rect(DISPLAY,HGREY,(25,135,200,50))
     pygame.draw.rect(DISPLAY,GREY,(20,130,200,50))
     DISPLAY.blit(font.render('Third', True, RED), (25, 135))
-
+    # Select
+    select = Rect(20, 10, 200, 50)
     while True:
-        UP = GPIO.input(23)
-        DOWN = GPIO.input(24)
-        if UP == False:
-            pygame.draw.rect(DISPLAY, RED, (FIRST, 200, 50), 3)
-        #if DOWN == False:
-        for event in pygame.event.get():
-            if event.type==QUIT:
-                pygame.quit()
-                sys.exit()
+        for (k,v) in button_map.items():
+            if GPIO.input(k) == False:
+                select.move_ip(v)
+        pygame.draw.rect(DISPLAY, RED, select, 3)
         pygame.display.update()
-
+        sleep(1)
 main()
+
